294d373798987b4f620e3da0065d70de1779c7c1
James R Hoggard III <jamesrhoggardiii@Jamess-MacBook-Air-2.local>
2022-08-05 23:31:07 -0400

    wireUpd0.2

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,9 @@
+class MessagesController < ApplicationController
+  def create
+    Message.create(message_params.merge({room_id: params[:room_id]}))
+  end
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
+end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,14 @@
 class Message < ApplicationRecord
   belongs_to :room
+
+  has_rich_text :content
+
+  after_create_commit :broadcast_to_room
+
+  private
+
+  def broadcast_to_room
+   room = self.room
+   broadcast_append_to "#{room.id}"
+  end
 end
